4041cfa0369992d13bbc257d4abf1d1a8d6c3d65
Atta Ur Rehman <bs.atta8123@gmail.com>
2015-03-23 16:53:51 +0500

    In Final Round

--- js/time.js
+++ js/time.js
@@ -1,1 +1,9 @@
-document.getElementById("demo").innerHTML = Date();
+
+var time = new Date();
+var showdata = "To Day Date :";
+var date= time.getDate();
+var date1 = time.getDay();
+var date2=time.getFullYear()
+var dayremaing = "FIVE DAYS REMAINING";
+document.getElementById("demo").innerHTML = showdata+" "+date+"/"+date1+"/"+date2+"<br>"+"<br>"+dayremaing+"<br>"+"<br>"+"HTML TEST ON 29/3/2015"+"<br>"+"<br>"+"BEST OF LUCK";
+
